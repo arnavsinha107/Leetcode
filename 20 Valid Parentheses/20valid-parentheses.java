44fd66b7b509abb10f82f5b23143afac28107966
BruhGunned <arnavsinha107@gmail.com>
2026-02-09 22:10:32 +0000

    Code challenge submission for question: 20 Valid Parentheses

---
+++ 20 Valid Parentheses/20valid-parentheses.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else {
+                if (st.isEmpty()) return false;
+                char t = st.pop();
+                if (c == ')' && t != '(') return false;
+                if (c == '}' && t != '{') return false;
+                if (c == ']' && t != '[') return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
