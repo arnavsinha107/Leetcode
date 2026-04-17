51af7b1bb9a6656bdb233ac66fb36195ed8b13ef
BruhGunned <arnavsinha107@gmail.com>
2026-04-17 18:18:41 +0000

    Code challenge submission for question: 973 K Closest Points to Origin

---
+++ 973 K Closest Points to Origin/973k-closest-points-to-origin.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->(b[0]*b[0] + b[1]*b[1]) - (a[0]*a[0] + a[1]*a[1]));
+        int n=points.length;
+        int[][] ans=new int[k][2];
+        
+        for(int i=0;i<n;i++){
+            pq.add(points[i]);
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        int i=0;
+        while(!pq.isEmpty()){
+            ans[i++]=pq.poll();
+        }
+        return ans;
+    }
+        
+}
