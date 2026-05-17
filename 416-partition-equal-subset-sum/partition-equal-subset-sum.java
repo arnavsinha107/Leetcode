e55906aa6ae9e3e75bd316285911b24de6d7dca9
Arnav <84615898+arnavsinha107@users.noreply.github.com>
2026-05-17 23:11:01 +0530

    Time: 54 ms (40.11%) | Memory: 47.7 MB (45.58%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -9,20 +9,40 @@ class Solution {
         }
         if(temp%2!=0)return false;
         int target=temp/2;
-        HashSet<Integer> dp=new HashSet<>();
-        dp.add(0);
-        for(int i=n-1;i>=0;i--){
-            HashSet<Integer> tempset=new HashSet<>();
-            for(int j:dp){
-                if(j+nums[i]==target)return true;
-                tempset.add(j+nums[i]);
-                tempset.add(j);
-            }
-            dp=tempset;
+        boolean[][] dp=new boolean[n+1][target+1];
+        for(int i=0;i<=n;i++){
+            dp[i][0]=true;
+
         }
-        if(dp.contains(target))return true;
-        else return false;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=target;j++){
+                if(j<nums[i-1]){
+                    dp[i][j]=dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=dp[i-1][j] || dp[i-1][j-nums[i-1]];
+                }
+            }
+        }
+        return dp[n][target];
         
     }
     
 }
+
+
+
+        // HashSet<Integer> dp=new HashSet<>();
+        // dp.add(0);
+        // for(int i=n-1;i>=0;i--){
+        //     HashSet<Integer> tempset=new HashSet<>();
+        //     for(int j:dp){
+        //         if(j+nums[i]==target)return true;
+        //         tempset.add(j+nums[i]);
+        //         tempset.add(j);
+        //     }
+        //     dp=tempset;
+        // }
+        // if(dp.contains(target))return true;
+        // else return false;
+        
