a0afc2584ef1409a99b3774edd95c582552fe873
Arnav <84615898+arnavsinha107@users.noreply.github.com>
2026-05-17 23:01:20 +0530

    Time: 561 ms (5.01%) | Memory: 49.6 MB (39.81%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        //int[] dp=new int[n+1];
+        int temp=0;
+        for(int i=0;i<n;i++){
+            temp+=nums[i];
+        }
+        if(temp%2!=0)return false;
+        int target=temp/2;
+        HashSet<Integer> dp=new HashSet<>();
+        dp.add(0);
+        for(int i=n-1;i>=0;i--){
+            HashSet<Integer> tempset=new HashSet<>();
+            for(int j:dp){
+                tempset.add(j+nums[i]);
+                tempset.add(j);
+            }
+            dp=tempset;
+        }
+        if(dp.contains(target))return true;
+        else return false;
+        
+    }
+    
+}
