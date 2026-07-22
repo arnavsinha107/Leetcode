44822760ba06e4ecd6480e0084251fb9a6572a41
Arnav <84615898+arnavsinha107@users.noreply.github.com>
2026-07-23 00:10:31 +0530

    Time: 3 ms (14.39%) | Memory: 47.2 MB (19.40%) - LeetSync

---
+++ 57-insert-interval/insert-interval.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        int l=0;
+        int r=intervals.length-1;
+        //int a=Interger.MAX_VALUE;
+        boolean flag=false;
+        int mid=0;
+        if(intervals.length==0){
+            return new int[][]{newInterval};
+        }
+        while(l<r && flag==false){
+            mid=l+(r-l)/2;
+            if(newInterval[0]==intervals[mid][0]){
+                flag=true;
+                
+            }
+            else if(newInterval[0]>intervals[mid][0]){
+                l=mid+1;
+                
+            }
+            else{
+                r=mid;
+            }
+        }
+        
+        ArrayList<int[]> ans=new ArrayList<>();
+        for(int i=0;i<intervals.length;i++){
+            if(i==mid){
+                ans.add(newInterval);
+            }
+            ans.add(intervals[i]);
+        }
+        
+        return merge(ans.toArray(new int[ans.size()][]));
+    }
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        List<int[]> ans=new ArrayList<>();
+        int start=intervals[0][0];
+        int end=intervals[0][1];
+        for(int i=1;i<intervals.length;i++){
+            if(intervals[i][0]<=end){
+                end=Math.max(end,intervals[i][1]);
+            }
+            else{
+                ans.add(new int[]{start,end});
+                start=intervals[i][0];
+                end=intervals[i][1];
+            }
+        }
+        ans.add(new int[]{start,end});
+        return ans.toArray(new int[ans.size()][]);
+    }
+}
