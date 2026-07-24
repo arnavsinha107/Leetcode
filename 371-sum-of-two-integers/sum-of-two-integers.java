955f33b2eeba86f4d068c01c99cf4bbf71200784
Arnav <84615898+arnavsinha107@users.noreply.github.com>
2026-07-25 00:59:28 +0530

    Time: 2 ms (0.52%) | Memory: 42.2 MB (33.47%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.java
+++ 371-sum-of-two-integers/sum-of-two-integers.java
@@ -1,5 +1,23 @@
 class Solution {
     public int getSum(int a, int b) {
-        return a+b;
+        String c=String.format("%32s",Integer.toBinaryString(a)).replace(' ','0');
+        String d=String.format("%32s",Integer.toBinaryString(b)).replace(' ','0');
+        StringBuilder binary=new StringBuilder();
+        int o=0;
+        for(int i=31;i>=0;i--){
+            int x=c.charAt(i)-'0';
+            int y=d.charAt(i)-'0';
+            int sum=x+y+o;
+            binary.append(sum%2);
+            o=sum/2;
+        }
+        binary.reverse();
+        int res=0;
+        for(int i=0;i<32;i++){
+            if(binary.charAt(31-i)=='1'){
+                res =res | (1<<i);
+            }
+        }
+        return res;
     }
 }
