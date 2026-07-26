49254819c7ff5e4a3525a3e68aadcec6575e16d4
Arnav <84615898+arnavsinha107@users.noreply.github.com>
2026-07-26 19:54:38 +0530

    Time: 1 ms (97.94%) | Memory: 47.5 MB (81.13%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,7 +1,10 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        HashSet<Integer> rows=new HashSet<>();
-        HashSet<Integer> cols=new HashSet<>();
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return;
+        }
+        boolean[] row=new boolean[matrix.length];
+        boolean[] col=new boolean[matrix[0].length];
 
         //boolean flag=false;
 
@@ -9,15 +12,14 @@ class Solution {
             for(int j=0;j<matrix[0].length;j++){
                 
                 if(matrix[i][j]==0){
-                    System.out.println(i+" "+j);
-                    rows.add(i);
-                    cols.add(j);
+                    row[i]=true;
+                    col[j]=true;
                 }
             }
         }
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
-                if(rows.contains(i)|| cols.contains(j)){
+                if(row[i]|| col[j]){
                     matrix[i][j]=0;
 
                 }
