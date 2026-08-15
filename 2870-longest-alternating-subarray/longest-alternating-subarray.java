2994ed73bd5131be8259c0bbc7629abc3eab4d88
Arnav <84615898+arnavsinha107@users.noreply.github.com>
2026-08-15 16:58:55 +0530

    Time: 1 ms (100.00%) | Memory: 46.2 MB (84.98%) - LeetSync

--- 2870-longest-alternating-subarray/longest-alternating-subarray.java
+++ 2870-longest-alternating-subarray/longest-alternating-subarray.java
@@ -1,38 +1,30 @@
 class Solution {
     public int alternatingSubarray(int[] nums) {
-        int j=0;
+        
         int ans=0;
         int n=nums.length;
-        //int l=0;
+        
         int curr=0;
-        int flag=0;
+       
         for(int i=1;i<n;i++){
             
-            if(nums[i]-nums[j]==Math.pow(-1,i+1)&& (curr>0 || nums[i]-nums[j]==1)){
-                if(flag==0){
-                    //System.out.println(nums[i]);
+           int diff=nums[i]-nums[i-1];
+           if(diff==1){
+                if(curr==0 || curr%2==0){
                     curr++;
                 }
-                else {
-                    flag=0;
+                else{
                     curr=1;
                 }
-            }
-            else if(nums[i]-nums[j]==Math.pow(-1,i) && (curr>0 || nums[i]-nums[j]==1)){
-                if(flag==1){
-                    //System.out.println(nums[i]-nums[j]);
-                    curr++;
-                }
-                else {
-                    flag=1;
-                    curr=1;
-                }
-            }
-            else{
+           }
+           else if(diff==-1 && curr>0 && curr%2==1){
+                curr++;
+           }
+           else{
                 curr=0;
-            }
-            j++;
-            ans=Math.max(ans,curr);
+           }
+           ans=Math.max(ans,curr);
+
         }
         return ans==0?-1:ans+1;
     }
